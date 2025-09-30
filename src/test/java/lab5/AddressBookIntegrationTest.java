c4b91d5fdb63782513692e0c0c05b0e1fe0f933b
haba111 <haniehbahrampour@cmail.carleton.ca>
2025-09-29 21:20:19 -0400

    changedsww

--- src/test/java/lab5/AddressBookIntegrationTest.java
+++ src/test/java/lab5/AddressBookIntegrationTest.java
@@ -3,6 +3,7 @@ package lab5;
 import lab5.BuddyInfo;
 import lab5.AddressBook;
 import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
